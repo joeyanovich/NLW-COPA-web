e8810a3a3f40e79fd217f2be77d9b7349ed5ed27
joeyanovich <joedison.dias@gmail.com>
2022-11-02 18:47:03 -0300

    :heavy_plus_sign:Add: axios library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,11 @@ import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 
-export default function Home() {
+interface HomeProps {
+  poolCount: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center">
     <main>
@@ -46,7 +50,7 @@ export default function Home() {
         <div className="flex items-center gap-6">
           <Image src={iconCheckImg} alt="" />
           <div className="flex flex-col">
-            <span className="font-bold text-2xl">+2.034</span>
+            <span className="font-bold text-2xl">+{props.poolCount}</span>
             <span>Bolões criados</span>
           </div>
         </div>
@@ -72,13 +76,16 @@ export default function Home() {
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch('http://localhost:3333/pools/count')
-//   const data = await response.json()
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
 
-//   return {
-//     props: {
-//       count: data.count,
-//     }
-//   }
-// }
+  const response = await fetch('http://localhost:3333/guesses/count')
+  const data = await response.json()
+
+  return {
+    props: {
+      poolCount: data.count,
+    }
+  }
+}
